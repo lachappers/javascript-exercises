034fecf66fb9b784f7dcb46487f759dc6ab3aecc
lachappers <116145040+lachappers@users.noreply.github.com>
2022-11-01 15:23:41 +0000

    return was in wrong place

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,15 @@
 const removeFromArray = function(theArr, ...theArgs) {
-   for (let arg of theArgs){
-        if (theArr.includes(arg,0)){
+   console.log(theArgs);
+
+    for (let arg of theArgs) {
+        if (theArr.includes(arg,0)) {
             popper = theArr.indexOf(arg,0);
             theArr.splice(popper,1);
-        }
-        return theArr;
+            }
+    }
+    return theArr;
 };
-removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 3, 4], 2, 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
