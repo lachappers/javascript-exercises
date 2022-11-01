430b2b69a788bd1e487ebf5ab26fece58c119fad
lachappers <116145040+lachappers@users.noreply.github.com>
2022-11-01 14:03:58 +0000

    passed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,13 +1,41 @@
 
 const repeatString = function(string, num) {
-    string = '';
-    for (let i = 0; i < num ; i++){
-        string += `hey`;
+    midString = '';
+    if (num <0){
+        midString = 'ERROR';
     }
-    return string;
+    else{
+        for (let i = 0; i < num ; i++){
+            midString += string;
+        }
+    }
+        return string = midString;
     
 };
 repeatString('hey',3);
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
+
+/* BAACKUP
+
+const repeatString = function(string, num) {
+   
+    if (num <0){
+        string = 'ERROR';
+    }
+    else{
+         for (let i = 0; i < num ; i++){
+            secondString += string;
+        }
+        return secondString;
+    }
+    
+};
+repeatString('hey',3);
+
+// Do not edit below this line
+module.exports = repeatString;
+*/
