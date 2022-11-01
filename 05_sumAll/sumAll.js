4c7eb6b9b54fff373b98077ebc13e1c75ebfcb9b
lachappers <116145040+lachappers@users.noreply.github.com>
2022-11-01 16:05:47 +0000

    used typeof not isNaN

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,14 @@
 const sumAll = function(first, last) {
-    let max = Math.max(first, last);
-    let min = Math.min(first, last);
-    sumOfAll = ((max-min)+1)*((min + max)/2);
-    return sumOfAll;
+    
+    if((typeof(first) !== 'number') || (typeof(last) !== 'number') || first < 0 || last <0) {
+        return sumOfAll = "ERROR"    
+    }
+    else {
+        let max = Math.max(first, last);
+        let min = Math.min(first, last);
+        sumOfAll = ((max-min)+1)*((min + max)/2);
+        return sumOfAll;
+    }
 };
 
 sumAll(1,4);
