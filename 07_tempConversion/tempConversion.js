c46963c001cac9ff8463077fdb385d873bf00443
lachappers <116145040+lachappers@users.noreply.github.com>
2022-11-01 21:08:23 +0000

    completed using Number and toFixed()

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  c = (f - 32)/1.8;
+  return Number(c.toFixed(1));
 };
+ftoc(32) // fahrenheit to celsius, should return 0
 
-const ctof = function() {
-
+const ctof = function(c) {
+  f = (c * 1.8)+32;
+  return Number(f.toFixed(1));
 };
+ctof(0) // celsius to fahrenheit, should return 32
 
 // Do not edit below this line
 module.exports = {
