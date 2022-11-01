404326e3f2792e83b44ba2d7fa68aa1553e8ea95
lachappers <116145040+lachappers@users.noreply.github.com>
2022-11-01 15:04:46 +0000

    multi value removal issues

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,12 @@
-const removeFromArray = function(theArr, theArgs) {
-    if (theArr.includes(theArgs,0)){
-        popper = theArr.indexOf(theArgs,0);
-        theArr.splice(popper,1);
-    }
-    
-    return theArr;
+const removeFromArray = function(theArr, ...theArgs) {
+   for (let arg of theArgs){
+        if (theArr.includes(arg,0)){
+            popper = theArr.indexOf(arg,0);
+            theArr.splice(popper,1);
+        }
+        return theArr;
 };
-removeFromArray([1, 2, 3, 4], 3, 5);
+removeFromArray([1, 2, 3, 4], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
