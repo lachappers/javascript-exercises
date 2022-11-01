d80c97b650668cacc000903abdac4449c7976e19
lachappers <116145040+lachappers@users.noreply.github.com>
2022-11-01 11:23:07 +0000

    finally understood function language

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
 
+const repeatString = function(string, times) {
+    
+    for (let i = 0; i < times ; i++){
+        string += `hey`;
+    }
+    return string;
+    
 };
+repeatString('hey',3);
 
 // Do not edit below this line
 module.exports = repeatString;
