a7ccd0c4a75e1b55ce18aaa997e3b47d5055dfe5
lachappers <116145040+lachappers@users.noreply.github.com>
2022-11-01 14:17:24 +0000

    passed first test with array and reverse array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let arr = input.split('');
+    let reverseInput = arr.reverse();
+    return reverseInput;
 };
+
+reverseString('hello there');
 
 // Do not edit below this line
 module.exports = reverseString;
