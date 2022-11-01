3efc6a611ba285c317d3e3e8c601c95c66705c58
lachappers <116145040+lachappers@users.noreply.github.com>
2022-11-01 15:40:20 +0000

    declared order of numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    let max = Math.max(first, last);
+    let min = Math.min(first, last);
+    sumOfAll = ((max-min)+1)*((min + max)/2);
+    return sumOfAll;
 };
+
+sumAll(1,4);
 
 // Do not edit below this line
 module.exports = sumAll;
