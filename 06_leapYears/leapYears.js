4a9ad7225a351abe791205b45874ec4aa705d558
lachappers <116145040+lachappers@users.noreply.github.com>
2022-11-01 19:04:29 +0000

    magical logical not genius

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
+const leapYears = function(leapYear) {
+    // divisible by 4, not divisible by 100, divisible by 400
+    let isLeapYear = ((leapYear % 4 ===0) && !(leapYear % 100 ===0) && (leapYear % 400 ===0));
+    return isLeapYear;
 
 };
+
+leapYears(2000) // is a leap year: returns true
+leapYears(1985)
 
 // Do not edit below this line
 module.exports = leapYears;
