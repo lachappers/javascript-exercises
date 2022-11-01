09218d8baffe6b9ee4b6cd8b6c905cfe2cc62751
lachappers <116145040+lachappers@users.noreply.github.com>
2022-11-01 20:56:58 +0000

    nested logical operators - logical not caused issues, now resolved

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,12 +1,28 @@
 const leapYears = function(leapYear) {
-    // divisible by 4, not divisible by 100, divisible by 400
-    let isLeapYear = ((leapYear % 4 ===0) && !(leapYear % 100 ===0) && (leapYear % 400 ===0));
-    return isLeapYear;
 
+   if ((leapYear % 4 === 0) && (leapYear % 100 !== 0) || (leapYear % 400 === 0)){
+    return isLeapYear = true;
+   }
+   else{
+    return isLeapYear = false;
+   }
 };
 
-leapYears(2000) // is a leap year: returns true
-leapYears(1985)
+// leapYears(2000) // is a leap year: returns true
+// leapYears(1985) // is not a leap year: returns false
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+    // else if (leapYear % 400 ===0) {
+    //     return isLeapYear = true;
+    // }
+    // else { 
+    //     return isLeapYear = false;}
+    // let isLeapYear = (
+    //     (leapYear % 4 ===0) && 
+    //     (
+    //         (leapYear % 400 ===0) &&
+    //         !!(leapYear % 100 ===0)
+    //         );
