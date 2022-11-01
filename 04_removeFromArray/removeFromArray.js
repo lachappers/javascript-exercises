a0db8cbca9c49fb2fc72518f3e2a02504eaa7e94
lachappers <116145040+lachappers@users.noreply.github.com>
2022-11-01 14:51:12 +0000

    splicing from an index one parameter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(theArr, theArgs) {
+    if (theArr.includes(theArgs,0)){
+        popper = theArr.indexOf(theArgs,0);
+        theArr.splice(popper,1);
+    }
+    
+    return theArr;
 };
+removeFromArray([1, 2, 3, 4], 3, 5);
 
 // Do not edit below this line
 module.exports = removeFromArray;
