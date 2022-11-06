df66c05e65fe8ea2fa7015922bd95795669f85c6
lachappers <116145040+lachappers@users.noreply.github.com>
2022-11-06 17:42:56 +0000

    removed punctuation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,6 @@
-const palindromes = function () {
-
-};
+const palindromes = (array) => {
+    processedArray = array.toLowerCase().replace(/[^a-z]/g, "");    return processedArray == processedArray.split('').reverse().join('');
+}
 
 // Do not edit below this line
 module.exports = palindromes;
